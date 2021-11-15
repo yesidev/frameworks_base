aeaf0b71ec94deeaac296a65a3ff40017b88362d
spkal01 <kalligeross@gmail.com>
2021-11-15 06:52:14 +0100

    Revert "SystemUI: Allow customizing footer text [1/2]"
    This reverts commit 29aaa6115159a7fc8c17666cee935d6a4c4eff06.

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -86,6 +86,5 @@ public class SystemSettings {
         Settings.System.NOTIFICATION_LIGHT_PULSE,
         Settings.System.DOZE_ON_CHARGE,
         Settings.System.QS_FOOTER_TEXT_SHOW,
-        Settings.System.QS_FOOTER_TEXT_STRING,
     };
 }
